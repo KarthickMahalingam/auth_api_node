63e5b1f18da6e4b49ef20f2b0e8a12b27d14ef31
Karthick Mahalingam <itkarthick@hotmail.com>
2018-07-22 17:07:42 -0700

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'Il0vePr0gramm1nG',
-    'database': 'mongodb://karthick:sastra123@ds137957.mlab.com:37957/network-prod'
+    'database': 'mongodb://${username}:${pwd}@ds137957.mlab.com:37957/network-prod'
 
 };
